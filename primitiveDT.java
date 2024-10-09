dc6e3415ec198a6ecdb4e33bfe431850063868e2
sapnajoshi-lab <joshisapna522@gmail.com>
2024-10-09 13:07:10 +0530

    Add files via upload

---
+++ primitiveDT.java
@@ -0,0 +1,26 @@
+package datatype;
+
+public class primitiveDT {
+
+	public static void main(String[] args) {
+		byte num=10;
+		short num1=4;
+		int num2=30;
+		long num3=1128;
+		char letter='s';
+		double xy=11.08;
+		boolean value=true;
+		
+		System.out.println(num);
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(letter);
+		System.out.println(xy);
+		System.out.println(value);
+		
+		
+
+	}
+
+}
