f904188ed9e49f9cd9896ce1fa13b02c21bcd93a
sapnajoshi-lab <joshisapna522@gmail.com>
2024-10-15 22:20:55 +0530

    Add files via upload

---
+++ RelationalOptr.java
@@ -0,0 +1,18 @@
+package operatorDemo;
+
+public class RelationalOptr {
+
+	public static void main(String[] args) {
+		int x=21;
+		int y=12;
+		boolean res1=x>=y;
+		System.out.println(res1);
+		boolean res2=x==y;
+		System.out.println(res2);
+		boolean res3=x<=y;
+		System.out.println(res3);
+		boolean res4=x!=y;
+		System.out.println(res4);
+	}
+
+}
