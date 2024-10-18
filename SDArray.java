08c352d131351b792094edb7f6948a8700bca831
sapnajoshi-lab <joshisapna522@gmail.com>
2024-10-18 12:28:03 +0530

    Add files via upload

---
+++ SDArray.java
@@ -0,0 +1,29 @@
+package ArrayDemo;
+
+public class SDArray {
+
+	public static void main(String[] args) {
+		String animals[]= {"Cat","Dog","Elephant","Lion","Tiger","Squirrel","Monkey","Mouse","Fox","Horse"};
+		System.out.println(animals);
+		for(int i=0;i<animals.length;i++)
+		{
+			System.out.println(animals[i]);
+		}
+			
+		int num[]=new int[5];
+		num[0]=15;
+		num[1]=25;
+		num[2]=35;
+		num[3]=45;
+		num[4]=55;
+		for (int i=0;i<5;i++)
+		{
+			System.out.println(num[i]);
+		
+		}
+
+	}
+
+}
+
+
