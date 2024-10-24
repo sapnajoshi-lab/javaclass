024797f249e92e93afc0746bf290607dba511424
sapnajoshi-lab <joshisapna522@gmail.com>
2024-10-24 13:25:35 +0530

    Add files via upload

---
+++ EmployeeData.java
@@ -0,0 +1,88 @@
+package variableDemo;
+
+class empname{
+	int Id;
+	String name;
+	int age;
+	double salary;
+}
+
+public class EmployeeData {
+
+	public static void main(String[] args) {
+
+			Empname em1=new Empname();
+			em1.Id=201;
+			em1.name="Sapana";
+			em1.age=20;
+			em1.salary=25000;
+			System.out.println("Id:"+em1.Id+" "+"Name:"+em1.name+" "+"Age:"+em1.age+" "+"Salary:"+em1.salary);
+			
+			Empname em2=new Empname();
+			em2.Id=202;
+			em2.name="Priya";
+			em2.age=20;
+			em2.salary=20000;
+			System.out.println("Id:"+em2.Id+" "+"Name:"+em2.name+" "+"Age:"+em2.age+" "+"Salary:"+em2.salary);
+			
+			Empname em3=new Empname();
+			em3.Id=2403;
+			em3.name="Jyotsana";
+			em3.age=21;
+			em3.salary=22000;
+			System.out.println("Id:"+em3.Id+" "+"Name:"+em3.name+" "+"Age:"+em3.age+" "+"Salary:"+em3.salary);
+			
+			Empname em4=new Empname();
+			em4.Id=204;
+			em4.name="Archana";
+			em4.age=26;
+			em4.salary=25000;
+			System.out.println("Id:"+em4.Id+" "+"Name:"+em4.name+" "+"Age:"+em4.age+" "+"Salary:"+em4.salary);
+			
+			Empname em5=new Empname();
+			em5.Id=205;
+			em5.name="Arjun";
+			em5.age=22;
+			em5.salary=24000;
+			System.out.println("Id:"+em5.Id+" "+"Name:"+em5.name+" "+"Age:"+em5.age+" "+"Salary:"+em5.salary);
+			
+			Empname em6=new Empname();
+			em6.Id=206;
+			em6.name="Ashish";
+			em6.age=21;
+			em6.salary=25000;
+			System.out.println("Id:"+em6.Id+" "+"Name:"+em6.name+" "+"Age:"+em6.age+" "+"Salary:"+em6.salary);
+			
+			Empname em7=new Empname();
+			em7.Id=207;
+			em7.name="Priti";
+			em7.age=20;
+			em7.salary=22000;
+			System.out.println("Id:"+em7.Id+" "+"Name:"+em7.name+" "+"Age:"+em7.age+" "+"Salary:"+em7.salary);
+			
+			Empname em8=new Empname();
+			em8.Id=208;
+			em8.name="Asha";
+			em8.age=21;
+			em8.salary=20000;
+			System.out.println("Id:"+em8.Id+" "+"Name:"+em8.name+" "+"Age:"+em8.age+" "+"Salary:"+em8.salary);
+			
+			Empname em9=new Empname();
+			em9.Id=209;
+			em9.name="Aditya";
+			em9.age=22;
+			em9.salary=23000;
+			System.out.println("Id:"+em9.Id+" "+"Name:"+em9.name+" "+"Age:"+em9.age+" "+"Salary:"+em9.salary);
+			
+			Empname em10=new Empname();
+			em10.Id=210;
+			em10.name="Chaitanya";
+			em10.age=23;
+			em10.salary=25000;
+			System.out.println("Id:"+em10.Id+" "+"Name:"+em10.name+" "+"Age:"+em10.age+" "+"Salary:"+em10.salary);
+		
+			
+		}
+
+	}
+		
